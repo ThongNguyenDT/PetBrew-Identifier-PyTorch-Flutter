52031e8e1da31aedf61ff7e9833a28f9887c94d6
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2024-03-15 12:18:41 +0700

    main work

---
+++ lib/widgets/app_bar/appbar_leading_iconbutton.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import '../../core/app_export.dart';
+import '../custom_icon_button.dart'; // ignore: must_be_immutable
+// ignore_for_file: must_be_immutable
+
+// ignore_for_file: must_be_immutable
+class AppbarLeadingIconbutton extends StatelessWidget {
+  AppbarLeadingIconbutton({Key? key, this.imagePath, this.margin, this.onTap})
+      : super(
+          key: key,
+        );
+
+  String? imagePath;
+
+  EdgeInsetsGeometry? margin;
+
+  Function? onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        onTap?.call();
+      },
+      child: Container(
+        decoration: AppDecoration.fillBlueGray,
+        padding: margin ?? EdgeInsets.zero,
+        child: CustomIconButton(
+          height: 34.v,
+          width: 38.h,
+          child: CustomImageView(
+            imagePath: ImageConstant.imgArrowDown,
+          ),
+        ),
+      ),
+    );
+  }
+}
