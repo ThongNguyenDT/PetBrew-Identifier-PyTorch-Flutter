a768558d4a8f62dab68676c4d3cffd4a2260949d
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2024-03-19 12:19:17 +0700

    main work

---
+++ lib/localization/en_us/en_us_translations.dart
@@ -0,0 +1,13 @@
+final Map<String, String> enUs = {
+  "msg_learn_more_about": "learn more about your pet",
+  "lbl_image_preview": "Image Preview",
+  "msg_browse_and_choose": "Browse and choose an image to search pet’s breed",
+  "msg_upload_your_image": "upload your image",
+  "lbl_app_navigation": "App Navigation",
+  "lbl_camera": "Camera",
+  "lbl_upload": "Upload",
+  "msg_check_your_app_s":
+      "Check your app's UI from the below demo screens of your app.",
+  "msg_network_err": "Network Error",
+  "msg_something_went_wrong": "Something Went Wrong!"
+};
