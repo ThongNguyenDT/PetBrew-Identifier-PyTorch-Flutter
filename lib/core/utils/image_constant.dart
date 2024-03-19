20fe4a60f54a34299bfffe5774c10864cf2f80cf
ThongNguyenDT <nguyenduongtienthong@gmail.com>
2024-03-19 12:19:19 +0700

    main work

---
+++ lib/core/utils/image_constant.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: must_be_immutable
+class ImageConstant {
+  // Image folder path
+  static String imagePath = 'assets/images';
+
+// Camera images
+  static String imgImage19 = '$imagePath/img_image_19.png';
+
+  static String imgGallery = '$imagePath/img_gallery.png';
+
+  static String imgSlatBtn1 = '$imagePath/img_slat_btn_1.png';
+
+// Upload images
+  static String imgPlus = '$imagePath/img_plus.svg';
+
+// Common images
+  static String imgArrowDown = '$imagePath/img_arrow_down.svg';
+
+  static String imageNotFound = 'assets/images/image_not_found.png';
+  static String rotage = 'assets/images/rotate-camera-right.svg';
+}
